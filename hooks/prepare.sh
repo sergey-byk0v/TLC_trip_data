8f24dbb3da4296a65414094b7989382e20c6183e
sergey-byk0v <mr.tuniguk@gmail.com>
2020-02-06 15:06:47 +0300

    change mv to cp

--- hooks/prepare.sh
+++ hooks/prepare.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo 'Instaling gitpython'
 pip3 install gitpython
-mv -f commit-msg ../.git/hooks/commit-msg
-mv -f pre-commit ../.git/hooks/pre-commit
-mv -f pre-push ../.git/hooks/pre-push
+cp -f commit-msg ../.git/hooks/commit-msg
+cp -f pre-commit ../.git/hooks/pre-commit
+cp -f pre-push ../.git/hooks/pre-push
