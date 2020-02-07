f83130c174329ec0e6ab217b19319e62b5e16954
sergey-byk0v <mr.tuniguk@gmail.com>
2020-02-07 19:09:22 +0300

    update get_stat.py like in previous branch

--- hw1/task1/get_stats.py
+++ hw1/task1/get_stats.py
@@ -1,84 +1,178 @@
 import pandas as pd
+from pandas.errors import ParserError
 import numpy as np
+import subprocess
+
+
+def to_datetime(x):
+    try:
+        x = np.datetime64(x)
+    except ValueError:
+        return None
+    else:
+        return x
+
+
+def to_int(x):
+    try:
+        x = int(x)
+    except ValueError:
+        return None
+    else:
+        return x
+
+
+def to_float(x):
+    try:
+        x = float(x)
+    except ValueError:
+        return None
+    else:
+        return x
+
+
+def read_value_data(path):
+    invalid_count = 0
+    while True:
+        try:
+            data = pd.read_csv(path)
+        except ParserError as error:
+            invalid_count += 1
+
+            error_line = str(error).split()[str(error).split().index('line') + 1][:-1]
+
+            command = f"sed -i {error_line}d {path}"
+            subprocess.call(command.split())
+        else:
+            return data, invalid_count
 
 
 def general_stats(path):
     gen_stat = {}
-    data = pd.read_csv(path)
-    columns = []
-    for column in data.columns:
-        columns.append(column.lower())
-    data.columns = columns
+    data, invalid_rows = read_value_data(path)
+    data.columns = np.vectorize(lambda s: s.lower())(data.columns)
+
+    data['lpep_dropoff_datetime'] = data['lpep_dropoff_datetime'].apply(to_datetime)
+    invalid_rows += data['lpep_dropoff_datetime'].isna().sum()
+
+    data['lpep_pickup_datetime'] = data['lpep_pickup_datetime'].apply(to_datetime)
+    invalid_rows += data['lpep_pickup_datetime'].isna().sum()
+
+    data['trip_distance'] = data['trip_distance'].apply(to_float)
+    invalid_rows += data['trip_distance'].isna().sum()
+
+    data['total_amount'] = data['total_amount'].apply(to_float)
+    invalid_rows += data['total_amount'].isna().sum()
+
+    data['passenger_count'] = data['passenger_count'].apply(to_int)
+    invalid_rows += data['passenger_count'].isna().sum()
+
+    data = data.dropna(subset=['lpep_pickup_datetime',
+                               'lpep_dropoff_datetime',
+                               'passenger_count',
+                               'trip_distance',
+                               'total_amount'])
+
     gen_stat['mean_cost'] = data['total_amount'].mean()
-    data['lpep_pickup_datetime'] = data['lpep_pickup_datetime'].astype(np.datetime64)
-    data['lpep_dropoff_datetime'] = data['lpep_dropoff_datetime'].astype(np.datetime64)
     data['trip_duration'] = data['lpep_dropoff_datetime'] - data['lpep_pickup_datetime']
     longest = data['trip_duration'].max()
     gen_stat['longest_ride'] = longest
 
     pickup_times = pd.DataFrame(data['lpep_pickup_datetime'])
-    pickup_times.index = pickup_times['lpep_pickup_datetime']
-    max_count_start = pickup_times['lpep_pickup_datetime'].resample('10T').count().idxmax()
-    max_count = pickup_times['lpep_pickup_datetime'].resample('10T').count().max()
-    max_count_end = max_count_start + np.timedelta64(10, 'm')
-    gen_stat['max_count'] = max_count
-    gen_stat['max_count_start'] = max_count_start
-    gen_stat['max_count_end'] = max_count_end
+    if pickup_times.shape[0] != 0:
+        pickup_times.index = pickup_times['lpep_pickup_datetime']
+        max_count_start = pickup_times['lpep_pickup_datetime'].resample('10T').count().idxmax()
+        max_count = pickup_times['lpep_pickup_datetime'].resample('10T').count().max()
+        max_count_end = max_count_start + np.timedelta64(10, 'm')
+        gen_stat['max_count'] = max_count
+        gen_stat['max_count_start'] = max_count_start
+        gen_stat['max_count_end'] = max_count_end
 
-    # invalid data where 0 passengers or 0 trip distance
-    missing_data_indices = np.logical_or(data['passenger_count'] == 0, data['trip_distance'] == 0)
-    invalid_rows = np.sum(missing_data_indices)
     gen_stat['invalid_rows'] = invalid_rows
     gen_stat = pd.DataFrame(gen_stat, index=[0])
-    return gen_stat, data
+    return gen_stat
 
 
-def missing_dates(path=None, data=None):
-    if data is None and path is not None:
-        data = pd.read_csv(path)
-    elif path is None and data is not None:
-        data = data
-    else:
-        return None
+def missing_dates(path):
 
-    missing_data_indices = np.unique(np.append(np.where(data['passenger_count'] == 0),
-                                               np.where(data['trip_distance'] == 0)))
-    missing_dates = pd.DataFrame(np.array(data['lpep_dropoff_datetime'])[missing_data_indices])
-    missing_dates.columns = ['missing_dates']
-    return missing_dates, data
+    missing_date = pd.Series()
+    data, _ = read_value_data(path)
+    data.columns = np.vectorize(lambda s: s.lower())(data.columns)
+
+    data['lpep_pickup_datetime'] = data['lpep_pickup_datetime'].apply(to_datetime)
+    data = data.dropna(subset=['lpep_pickup_datetime'])
+
+    data['lpep_dropoff_datetime'] = data['lpep_dropoff_datetime'].apply(to_datetime)
+    missing_date = missing_date.append(
+        data['lpep_pickup_datetime'].iloc[np.where(data['lpep_dropoff_datetime'].isna())[0]].apply(lambda i: i.date()),
+        ignore_index=True)
+
+    data['trip_distance'] = data['trip_distance'].apply(to_float)
+    missing_date = missing_date.append(
+        data['lpep_pickup_datetime'].iloc[np.where(data['trip_distance'].isna())[0]].apply(lambda i: i.date()),
+        ignore_index=True)
+
+    data['total_amount'] = data['total_amount'].apply(to_float)
+    missing_date = missing_date.append(
+        data['lpep_pickup_datetime'].iloc[np.where(data['total_amount'].isna())[0]].apply(lambda i: i.date()),
+        ignore_index=True)
+
+    data['passenger_count'] = data['passenger_count'].apply(to_int)
+    missing_date = missing_date.append(
+        data['lpep_pickup_datetime'].iloc[np.where(data['passenger_count'].isna())[0]].apply(lambda i: i.date()),
+        ignore_index=True)
+
+    data = data.dropna(subset=['lpep_dropoff_datetime',
+                               'passenger_count',
+                               'trip_distance',
+                               'total_amount'])
+    valid_dates = data['lpep_pickup_datetime'].apply(lambda i: i.date())
+    missing_date = pd.DataFrame(missing_date.apply(lambda s: None if s in valid_dates.values else s).dropna(),
+                                columns=['missing_dates'])
+
+    return missing_date
 
 
-def usage_stat(path=None, data=None):
-    if data is None and path is not None:
-        data = pd.read_csv(path)
-    elif path is None and data is not None:
-        data = data
-    else:
-        return None
-    invalid_indices = np.logical_or(data['passenger_count'] == 0, data['trip_distance'] == 0)
-    data['lpep_dropoff_datetime'] = pd.to_datetime(data['lpep_dropoff_datetime'])
-    valid_indices = np.logical_not(invalid_indices)
-    stat = data[valid_indices].groupby(data['lpep_dropoff_datetime'].dt.date)['vendorid'].count()
+def usage_stat(path=None):
+    data, _ = read_value_data(path)
+    data.columns = np.vectorize(lambda s: s.lower())(data.columns)
+
+    data['lpep_dropoff_datetime'] = data['lpep_dropoff_datetime'].apply(to_datetime)
+    data['lpep_pickup_datetime'] = data['lpep_pickup_datetime'].apply(to_datetime)
+    data['trip_distance'] = data['trip_distance'].apply(to_float)
+    data['passenger_count'] = data['passenger_count'].apply(to_int)
+    data = data.dropna(subset=['lpep_pickup_datetime',
+                               'lpep_dropoff_datetime',
+                               'passenger_count',
+                               'trip_distance',
+                               'total_amount'])
+
+    stat = data.groupby(data['lpep_pickup_datetime'].dt.date)['vendorid'].count()
     stat = pd.DataFrame(stat)
+    stat.index.name = 'date'
     stat.columns = ['count']
-    return stat, data
+    return stat
 
 
-def trip_stat(path=None, data=None):
-    if data is None and path is not None:
-        data = pd.read_csv(path)
-    elif path is None and data is not None:
-        data = data
+def trip_stat(path):
+    data, _ = read_value_data(path)
+    data.columns = np.vectorize(lambda s: s.lower())(data.columns)
+
+    data['lpep_dropoff_datetime'] = data['lpep_dropoff_datetime'].apply(to_datetime)
+    data['lpep_pickup_datetime'] = data['lpep_pickup_datetime'].apply(to_datetime)
+    data['trip_distance'] = data['trip_distance'].apply(to_float)
+    data['passenger_count'] = data['passenger_count'].apply(to_int)
+    data = data.dropna(subset=['lpep_pickup_datetime',
+                               'lpep_dropoff_datetime',
+                               'passenger_count',
+                               'trip_distance',
+                               'total_amount'])
+
+    data['month'] = data['lpep_pickup_datetime'].apply(lambda d: d.month)
+
+    if data['lpep_pickup_datetime'].shape[0] != 0:
+        trip_stat = pd.DataFrame(data.groupby(['month', 'passenger_count'])['trip_distance'].mean())
     else:
-        return None
-    person_count = data['passenger_count']
-    person_count.index = data['lpep_dropoff_datetime'].astype(np.datetime64)
-    trip_stat = pd.DataFrame(person_count.groupby(by=[person_count.index.year, person_count.index.month]).sum())
-    trip_duration = data['passenger_count']
-    trip_duration.index = data['lpep_dropoff_datetime'].astype(np.datetime64)
-    trip_stat['trip_duration'] = trip_duration.groupby(by=[trip_duration.index.year, trip_duration.index.month]).mean()
-    trip_stat['count'] = trip_duration.groupby(by=[trip_duration.index.year, trip_duration.index.month]).count()
-    trip_stat.index.name = 'date'
-    trip_stat.columns = ['average_passenger', 'mean_trip_duration', 'count']
+        trip_stat = pd.DataFrame(data.groupby(['month', 'passenger_count'])['trip_distance'].count())
 
-    return trip_stat, data
+    return trip_stat
