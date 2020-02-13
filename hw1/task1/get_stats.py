994c54ad51d94d0c028b60cb3e9a4f65fb4617e4
HacketOS <mr.tuniguk@gmail.com>
2020-02-13 23:18:51 +0300

    fix output formats and error for old data

--- hw1/task1/get_stats.py
+++ hw1/task1/get_stats.py
@@ -1,52 +1,73 @@
 import pandas as pd
 import numpy as np
+import csv
 
 
-def convert_type_function(type_to_convert, return_if_exception=None):
+def convert_type(type_to_convert, return_if_exception=None):
     def return_function(x):
         try:
             x = type_to_convert(x)
-        except ValueError:
+        except Exception:
             return return_if_exception
-        else:
-            return x
+        return x
     return return_function
 
 
+def get_time(datetime):
+    day_index = 0
+    datetime_str = str(datetime).split()
+    if datetime_str[day_index] == '0':
+        return datetime_str[-1]
+    else:
+        return str(datetime)
+
+
+def valid_duration(duration):
+    if duration < 0:
+        return None
+    else:
+        return duration
+
+
 def read_value_data(path):
-    data = pd.read_csv(path, error_bad_lines=False)
+    data = []
+    with open(path) as csv_file:
+        reader = csv.reader(csv_file)
+        header = reader.__next__()
+        header_len = len(header)
+        for row in reader:
+            data.append(row[:header_len])
+
+    data = pd.DataFrame(data, columns=header)
     data.columns = np.char.lower(np.array(data.columns, dtype=str))
 
-    data['lpep_dropoff_datetime'] = data['lpep_dropoff_datetime'].apply(convert_type_function(np.datetime64))
-    data['lpep_pickup_datetime'] = data['lpep_pickup_datetime'].apply(convert_type_function(np.datetime64))
-    data['trip_distance'] = data['trip_distance'].apply(convert_type_function(float))
-    data['total_amount'] = data['total_amount'].apply(convert_type_function(float))
-    data['passenger_count'] = data['passenger_count'].apply(convert_type_function(int))
-    return data
-
-
-def general_stats(path=None, data=None):
-    if data is None and path is not None:
-        data = read_value_data(path)
-    elif path is None and data is not None:
-        data = data
-    else:
-        return None
-
-    gen_stat = {}
     required_columns = ['lpep_pickup_datetime',
                         'lpep_dropoff_datetime',
-                        'total_amount',
                         'passenger_count',
-                        'trip_distance']
+                        'trip_distance',
+                        'total_amount',
+                        'trip_duration']
 
-    invalid_rows = np.any(data[required_columns].isna(), axis=1).sum()
+    data['lpep_pickup_datetime'] = data['lpep_pickup_datetime'].apply(convert_type(np.datetime64))
+    data['lpep_dropoff_datetime'] = data['lpep_dropoff_datetime'].apply(convert_type(np.datetime64))
+    data['trip_duration'] = data['lpep_dropoff_datetime'] - data['lpep_pickup_datetime']
+    data['trip_duration'] = data['trip_duration'].dt.total_seconds()
+    data['trip_duration'] = data['trip_duration'].apply(valid_duration)
+    data['trip_distance'] = data['trip_distance'].apply(convert_type(float))
+    data['total_amount'] = data['total_amount'].apply(convert_type(float))
+    data['passenger_count'] = data['passenger_count'].apply(convert_type(int))
 
-    clean_data = data[required_columns].dropna()
+    return data[required_columns]
+
+
+def general_stats(data, return_count=True):
+    gen_stat = {}
+    invalid_rows = np.any(data.isna(), axis=1).sum()
+    clean_data = data.dropna()
 
     gen_stat['mean_cost'] = clean_data['total_amount'].mean()
-    clean_data['trip_duration'] = clean_data['lpep_dropoff_datetime'] - clean_data['lpep_pickup_datetime']
-    longest = clean_data['trip_duration'].max()
+    clean_data['trip_duration'] = pd.to_timedelta(clean_data['trip_duration'], unit='s').apply(get_time)
+    longest = get_time(clean_data['trip_duration'].max())
     gen_stat['longest_ride'] = longest
 
     pickup_times = pd.DataFrame(clean_data['lpep_pickup_datetime'])
@@ -61,25 +82,15 @@ def general_stats(path=None, data=None):
         gen_stat['max_count_end'] = max_count_end
 
     gen_stat['invalid_rows'] = invalid_rows
-    gen_stat['count'] = clean_data.shape[0]
+    if return_count:
+        gen_stat['count'] = clean_data.shape[0]
     gen_stat = pd.DataFrame(gen_stat, index=[0])
     return gen_stat
 
 
-def missing_dates(path=None, data=None):
-    if data is None and path is not None:
-        data = read_value_data(path)
-    elif path is None and data is not None:
-        data = data
-    else:
-        return None
-    required_columns = ['lpep_pickup_datetime',
-                        'lpep_dropoff_datetime',
-                        'passenger_count',
-                        'trip_distance',
-                        'total_amount']
+def missing_dates(data):
 
-    clean_data = data[required_columns].dropna()
+    clean_data = data.dropna()
     max_time = clean_data['lpep_pickup_datetime'].max()
     min_time = clean_data['lpep_pickup_datetime'].min()
     missing_date = pd.Series(pd.date_range(min_time, max_time, freq='D').date)
@@ -91,22 +102,8 @@ def missing_dates(path=None, data=None):
     return missing_date
 
 
-def usage_stat(path=None, data=None):
-    if data is None and path is not None:
-        data = read_value_data(path)
-    elif path is None and data is not None:
-        data = data
-    else:
-        return None
-
-    print(data.shape)
-    required_columns = ['lpep_pickup_datetime',
-                        'lpep_dropoff_datetime',
-                        'total_amount',
-                        'passenger_count',
-                        'trip_distance']
-
-    clean_data = data[required_columns].dropna()
+def usage_stat(data):
+    clean_data = data.dropna()
     stat = clean_data.groupby(clean_data['lpep_pickup_datetime'].dt.date)['lpep_pickup_datetime'].count()
     stat = pd.DataFrame(stat)
     stat.index.name = 'date'
@@ -114,26 +111,14 @@ def usage_stat(path=None, data=None):
     return stat
 
 
-def trip_stat(path=None, data=None):
-    if data is None and path is not None:
-        data = read_value_data(path)
-    elif path is None and data is not None:
-        data = data
-    else:
-        return None
-
-    required_columns = ['lpep_pickup_datetime',
-                        'lpep_dropoff_datetime',
-                        'passenger_count',
-                        'trip_distance',
-                        'total_amount']
-
-    clean_data = data[required_columns].dropna()
-
+def trip_stat(data):
+    clean_data = data.dropna()
+    clean_data['passenger_count'] = clean_data['passenger_count'].astype(int)
     clean_data['month'] = clean_data['lpep_pickup_datetime'].apply(lambda d: d.month)
 
-    if len(data['lpep_pickup_datetime']) != 0:
-        trip_stat = pd.DataFrame(clean_data.groupby(['month', 'passenger_count'])['trip_distance'].mean())
+    if len(clean_data['lpep_pickup_datetime']) != 0:
+        trip_stat = pd.DataFrame(clean_data.groupby(['month', 'passenger_count'])['trip_duration'].mean())
+        trip_stat['trip_duration'] = pd.to_timedelta(trip_stat['trip_duration'].apply(int), unit='s').apply(get_time)
     else:
         trip_stat = pd.DataFrame(['month', 'passenger_count'])
 
