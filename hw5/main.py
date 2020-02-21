e56c583eba60ee09f81628f9e75ea8f804fbdc0c
HacketOS <mr.tuniguk@gmail.com>
2020-02-21 12:58:43 +0300

    fix issues in pr

--- hw5/main.py
+++ hw5/main.py
@@ -6,14 +6,15 @@ from files import read_csv, write_to_csv
 
 
 def _main():
-    columns, data = read_csv(sys.argv[1])
+    _, data = read_csv(sys.argv[1])
     users = [d[3] for d in data]
     prices = [float(d[2]) for d in data]
     stores = [d[1] for d in data]
     items = [d[0] for d in data]
-    print(f'Unique value of stores: {len(unique_count(stores))}')
-    print(f'Unique value of items: {len(unique_count(items))}')
-    print(f'User with max price count: {max(unique_count(users), key=unique_count(users).get)}')
+    print(f'Unique value of stores: {len(set(stores))}')
+    print(f'Unique value of items: {len(set(items))}')
+    unique_users = unique_count(users)
+    print(f'User with max price count: {max(unique_users, key=unique_users.get)}')
 
     print(f'~~~Amount of items in each store~~~')
     stores_count = unique_count(stores)
